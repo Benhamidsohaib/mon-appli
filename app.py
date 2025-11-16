109d29d36b8fe4915f280bb11e61c363a2560fcd
ben-hamid <Sohaib.aitbella33@gmail.com>
2025-11-16 01:37:53 +0100

    mise à jour

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ HTML = """
   <!-- deux petites vignettes en haut -->
   <section class="grid-top">
     <div class="box small">
-      <img src="https://fr.freepik.com/vecteurs-libre/voitures-usines-grande-ville-font-fumee-sale_5837846.htm#fromView=search&page=1&position=0&uuid=00fbdff7-8ae1-42c8-a93e-fcf66b79a40d&query=voiture+polluante" alt="Poubelle déposée">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Fcar-emitting-carbon-dioxide-co-environmental-pollution-problem-vector-illustration-isolated-white-background-124259733.jpg&f=1&nofb=1&ipt=d91c33fa5278a4c60968589c59d3d39a9dcd2c658f499fa4c1fb55cf10544446" alt="voiture pollution">
       <div class="card-body">
         <h3>Photo d'une poubelle déposée</h3>
         <p class="note">Essence polluante</p>
@@ -112,7 +112,7 @@ HTML = """
     </div>
 
     <div class="box small">
-      <img src="https://fr.freepik.com/vecteurs-libre/composition-dechets-organiques_26764821.htm#fromView=search&page=1&position=3&uuid=e381b856-2515-44e1-9c83-2cb74ac469af&query=poubelle+pleine" alt="Poubelle débordante">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fvecteurs-libre%2Fpoubelle-debordante-ordures-menageres-restes_376504-6.jpg%3Fsize%3D626%26ext%3Djpg&f=1&nofb=1&ipt=884b10f01d118cd843823cdf5436a0339de66c17b9d7eac5ffe0f904b9c6133e">
       <div class="card-body">
         <h3>Poubelle  (remplie)</h3>
         <p class="note">Poubelle qui déborde</p>
@@ -123,7 +123,7 @@ HTML = """
   <!-- deux grandes vignettes en bas -->
   <section class="grid-bottom">
     <div class="box large">
-      <img src="https://fr.freepik.com/vecteurs-libre/illustration-concept-abstrait-voyage-terrain-voyage-scolaire-excursion-pour-eleves-voyage-groupe-etudiants-decouverte-nature-visite-experience-culturelle-activite-processus-scolaire_12145624.htm#fromView=search&page=2&position=10&uuid=9979704c-2067-4ab4-8bdc-56bf4a87ec54&query=bus" alt="Transports en commun">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fvecteurs-premium%2Fdessin-bus-descendant-route-montagnes-arriere-plan_1157866-1253.jpg&f=1&nofb=1&ipt=4bd3ce37bc5db28f05b7de62489e08eb389db3f5953928f5a3764ce51faf4863" alt="Transports en commun">
       <div class="card-body">
         <h3>Prioriser les transports en commun</h3>
         <p class="note">Mobilité durable — image libre Pixabay</p>
@@ -131,7 +131,7 @@ HTML = """
     </div>
 
     <div class="box large">
-      <img src="https://images.pexels.com/photos/2873277/pexels-photo-2873277.jpeg" alt="Pollution plastique océan">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Fd%25C3%25A9chets-plastiques-de-dans-l-eau-mer-oc%25C3%25A9an-illustration-vecteur-plastique-la-avec-des-dessin-d-environnement-pollu%25C3%25A9-202922331.jpg&f=1&nofb=1&ipt=5ed8f954c12b8842c62d930525109a20e4ec6056c08f7b2a822b3abff1f47283" alt="Pollution plastique océan">
       <div class="card-body">
         <h3>Océan avec accumulation de plastique</h3>
         <p class="note">Pollution marine — image libre Pexels</p>
